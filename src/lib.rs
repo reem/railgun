00aa7c834f9ad0789eb504548fd373280fb98f63
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-18 18:03:33 -0800

    (init)

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![license = "MIT"]
+#![deny(missing_docs, warnings)]
+
+//! Fast, safe middleware built on top of Hyper.
+
+extern crate hyper;
+extern crate modifier;
+extern crate typemap;
+extern crate reduce;
+extern crate plugin;
+extern crate error;
+extern crate url;
+
