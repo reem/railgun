cc3539147554a714e230690fcb2b3c5826ee6be7
Jonathan Reem <jonathan.reem@gmail.com>
2014-11-20 20:13:36 -0800

    (feat) Streaming Response.

--- src/response.rs
+++ src/response.rs
@@ -1,17 +1,42 @@
-use hyper::{server, net};
+use std::io::IoResult;
+
+use hyper::server;
 use typemap::TypeMap;
 
-pub struct Response<'a, W> {
+use {RailgunResult, Fresh, Streaming};
+
+pub struct Response<'a, W = Fresh> {
     pub extensions: TypeMap,
     hyper_res: server::Response<'a, W>
 }
 
-impl<'a> Response<'a, net::Fresh> {
-    pub fn lift(res: server::Response<'a, net::Fresh>) -> Response<'a, net::Fresh> {
+impl<'a> Response<'a, Fresh> {
+    pub fn lift(res: server::Response<'a, Fresh>) -> Response<'a, Fresh> {
         Response {
             extensions: TypeMap::new(),
             hyper_res: res
         }
+    }
+
+    pub fn start(self) -> RailgunResult<Response<'a, Streaming>> {
+        Ok(Response {
+            extensions: self.extensions,
+            hyper_res: try!(self.hyper_res.start())
+        })
+    }
+}
+
+impl<'a> Response<'a, Streaming> {
+    pub fn end(self) -> RailgunResult<()> {
+        try!(self.hyper_res.end());
+        Ok(())
+    }
+
+}
+
+impl<'a> Writer for Response<'a, Streaming> {
+    fn write(&mut self, buf: &[u8]) -> IoResult<()> {
+        self.hyper_res.write(buf)
     }
 }
 
